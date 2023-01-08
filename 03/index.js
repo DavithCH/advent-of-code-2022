0dd5566fbd0e984eaa086f5fa97105a87d25203a
davith chhung <davithch@daviths-MBP.lan>
2023-01-08 21:41:48 +0100

    complete day03

--- 03/index.js
+++ 03/index.js
@@ -42,4 +42,37 @@ fs.readFile("./list.txt", "utf8", (err, data) => {
   }
 
   sumOfpriorities();
+
+  function part2() {
+    let sum = 0;
+
+    for (let i = 0; i < rucksacks.length; i += 3) {
+      // create backpacks of tree
+      const backpacks = [
+        [...rucksacks[i]],
+        [...rucksacks[i + 1]],
+        [...rucksacks[i + 2]],
+      ];
+      // create set of first backpack
+      let set = new Set(backpacks[0]);
+
+      // find intersection between first and second backpack
+      let intersections = backpacks[1].filter((item) => set.has(item));
+
+      // create set of intersection of first and second backpack
+      set = new Set(intersections);
+
+      // find intersection between all tree backpacks
+      intersections = backpacks[2].filter((item) => set.has(item));
+
+      //remove duplicates
+      const uniqueIntersections = [...new Set(intersections)];
+
+      sum += convertLetterToPriority(uniqueIntersections[0]);
+    }
+
+    console.log(sum);
+  }
+
+  part2();
 });
