dea6988d347be46b6abefaef3472f2d7600c2916
davith chhung <davithch@daviths-MBP.lan>
2023-01-08 22:23:37 +0100

    complete day04

--- 04/index.js
+++ 04/index.js
@@ -30,4 +30,27 @@ fs.readFile("./list.txt", "utf8", (err, data) => {
   }
 
   countAssignmentPairs();
+
+  function countAssignmentPairsOverlap() {
+    // loop over the list for each one we seperate to two intervals then each interval we separe to get numbers
+    const result = lines.map((line) => {
+      const [intervalOne, intervalTwo] = line
+        .split(",")
+        .map((interval) => interval.split("-").map(Number))
+        .sort((a, b) => {
+          const firstSize = a[1] - a[0];
+          const secondSize = b[1] - b[0];
+
+          return secondSize - firstSize;
+        });
+
+      const overlap =
+        intervalOne[1] >= intervalTwo[0] && intervalTwo[1] >= intervalOne[0];
+      return overlap ? 1 : 0;
+    });
+
+    console.log(result.reduce((a, b) => a + b, 0));
+  }
+
+  countAssignmentPairsOverlap();
 });
