50fee59d6207f0b3d91b5cd24962f8bc13339ad2
davith chhung <davithch@daviths-MBP.lan>
2023-01-08 23:31:51 +0100

    complete day06

--- 06/index.js
+++ 06/index.js
@@ -10,17 +10,38 @@ fs.readFile("./list.txt", "utf8", (err, data) => {
   function isUnique(array) {
     return new Set(array).size === array.length;
   }
+  function part1() {
+    let slidingWindow = [];
+    for (let i = 0; i < input.length; i++) {
+      slidingWindow.push(input[i]);
+      if (slidingWindow.length > 4) {
+        slidingWindow.shift();
+      }
 
-  let slidingWindow = [];
-  for (let i = 0; i < input.length; i++) {
-    slidingWindow.push(input[i]);
-    if (slidingWindow.length > 4) {
-      slidingWindow.shift();
-    }
-
-    if (slidingWindow.length === 4 && isUnique(slidingWindow)) {
-      console.log(i + 1);
-      break;
+      if (slidingWindow.length === 4 && isUnique(slidingWindow)) {
+        console.log(i + 1);
+        break;
+      }
     }
   }
+
+  part1();
+
+  function part2() {
+    const windowLength = 14;
+    let slidingWindow = [];
+    for (let i = 0; i < input.length; i++) {
+      slidingWindow.push(input[i]);
+      if (slidingWindow.length > windowLength) {
+        slidingWindow.shift();
+      }
+
+      if (slidingWindow.length === windowLength && isUnique(slidingWindow)) {
+        console.log(i + 1);
+        break;
+      }
+    }
+  }
+
+  part2();
 });
