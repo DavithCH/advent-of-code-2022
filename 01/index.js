de53318124986234c6557566056e3bc0ee567137
davith chhung <davithch@daviths-MBP.lan>
2023-01-08 19:35:22 +0100

    complete day01

--- 01/index.js
+++ 01/index.js
@@ -16,4 +16,23 @@ fs.readFile("./list.txt", "utf8", (err, data) => {
     console.log(Math.max(...highestCalories));
   }
   getHighestCalories();
+
+  function getSumTopThreeHighestCalories() {
+    const highestCalories = elves.map((elf) => {
+      const calories = elf.split("\n").map(Number);
+      return calories.reduce((prev, current) => prev + current, 0);
+    });
+
+    // classic sorting algo
+    highestCalories.sort((a, b) => b - a);
+
+    //only take first 3 highest
+    let sumOfThreeHigestCalories = highestCalories
+      .slice(0, 3)
+      .reduce((prev, current) => prev + current, 0);
+
+    console.log(sumOfThreeHigestCalories);
+  }
+
+  getSumTopThreeHighestCalories();
 });
